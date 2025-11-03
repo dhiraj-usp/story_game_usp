50289504736f86a8c824478385b32a06a1bd44bb
Akash SK <akashsk.usp@gmail.com>
2025-11-03 16:32:57 +0530

    added interactions

--- Assets/USP/Addressables/Catalogs/MiniGames/Dogfeeding Game/Scripts/GameLogic/MiniInteractions.cs
+++ Assets/USP/Addressables/Catalogs/MiniGames/Dogfeeding Game/Scripts/GameLogic/MiniInteractions.cs
@@ -14,6 +14,12 @@ namespace USP.Minigame.DF_Game
         [SerializeField] private ParticleSystem treeParticles;
         [SerializeField] private List<CameraAnchor> cameraAnchors;
 
+        [SerializeField] private Animator postboxanimator;
+        [SerializeField] private ClikcableObject postbox;
+
+        [SerializeField] private Animator boneanimator;
+        [SerializeField] private ClikcableObject bone;
+
         public void DisableDraggableAnchors()
         {
             foreach (var cameraAnchor in cameraAnchors)
@@ -34,6 +40,8 @@ namespace USP.Minigame.DF_Game
             waterpot.OnTiltThresholdnotReached += StopWaterParticles;
             foodbowl.OnClick += OnclikcedFoodBowl;
             tree.OnClick += Onclikcedtree;
+            postbox.OnClick += OnclickedPostbox;
+            bone.OnClick += OnclickedBone;
         }
 
         public void PlayWaterParticles()
@@ -53,6 +61,15 @@ namespace USP.Minigame.DF_Game
         public void Onclikcedtree()
         {
             treeParticles.Play();
+        }
+
+        public void OnclickedPostbox()
+        {
+            postboxanimator.SetTrigger("open");
+        }
+        public void OnclickedBone()
+        {
+            boneanimator.SetTrigger("open");
         }
     }
 }
